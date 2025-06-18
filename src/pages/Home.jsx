2b72fd4c78dc414bb9d6a75cb0b9059d0899a2f5
Steddy <138579152+Steddyz@users.noreply.github.com>
2025-06-18 10:53:56 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import About from "../components/About";
 import Skills from "../components/Skills";
+import Projects from "../components/Projects";
 
 const Home = () => {
   return (
@@ -16,6 +17,7 @@ const Home = () => {
         <Hero />
         <About />
         <Skills />
+        <Projects />
       </main>
     </div>
   );
