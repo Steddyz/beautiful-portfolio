b9fe4a3d50e39d1bd3e238d56df7f3f32a140928
Steddy <138579152+Steddyz@users.noreply.github.com>
2025-06-18 10:48:13 +0300

    add data projects

---
+++ src/components/Projects.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+const projects = [
+  {
+    id: 1,
+    title: "Architectural workshop",
+    description:
+      "Welcome to our Architectural Workshop, where creativity meets functionality, powered by React and JavaScript.",
+    image: "/projects/architectProject.png",
+    tags: ["React", "JavaScript", "HTML", "CSS", "Axios", "Vite"],
+    demoUrl: "#",
+  },
+  {
+    id: 2,
+    title: "Evogym fitness",
+    description:
+      "Welcome to Evogym Fitness, your ultimate destination for health and wellness.",
+    image: "/projects/gymProject.png",
+    tags: ["React", "TypeScript", "TailwindCSS", "HTML", "Vite"],
+    demoUrl: "#",
+  },
+  {
+    id: 3,
+    title: "Recipe by Steddy",
+    description:
+      "Discover culinary inspiration with Recipe by Steddy, your go-to source for delicious and easy-to-follow recipes.",
+    image: "/projects/recipesProject.png",
+    tags: ["React", "JavaScript", "HTML", "CSS", "Axios", "Vite"],
+    demoUrl: "#",
+  },
+  {
+    id: 4,
+    title: "Hilink Travels",
+    description:
+      "Welcome to Hilink Travels, your trusted partner in crafting unforgettable travel experiences.",
+    image: "/projects/travelProject.png",
+    tags: ["React", "TypeScript", "Next.js", "HTML", "TailwindCSS", "Vite"],
+    demoUrl: "#",
+  },
+  {
+    id: 5,
+    title: "Pokemon Pokedex",
+    description:
+      "Step into the enchanting world of Pokemon with the Pokemon Pokedex, your comprehensive resource for all things Pokemon!",
+    image: "/projects/pokedexProject.png",
+    tags: ["React", "TypeScript", "HTML", "Axios", "CSS", "Vite"],
+    demoUrl: "#",
+  },
+  {
+    id: 6,
+    title: "Car Rental Shop",
+    description:
+      "Discover the freedom of the open road with Car Rental Shop, your premier choice for car rentals!",
+    image: "/projects/carRentalProject.png",
+    tags: ["React", "TypeScript", "Next.js", "HTML", "TailwindCSS", "Vite"],
+    demoUrl: "#",
+  },
+];
+
+const Projects = () => {
+  return <section>Projects</section>;
+};
+
+export default Projects;
